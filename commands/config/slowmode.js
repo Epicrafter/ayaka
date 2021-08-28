891c9de385e52ebfed5ad1cca220cbdff7614f81
Epicrafter <68658005+Epicrafter@users.noreply.github.com>
2021-08-28 11:50:47 +0300

    😎 | Slowmode tweeking

--- commands/config/slowmode.js
+++ commands/config/slowmode.js
@@ -12,34 +12,22 @@ module.exports = {
         let mention = args[1];
 
         let regex = /\d/;
-        let channel = message.guild.channels.cache.get(mention);
+
+        duration == 'off' || duration == '0s' ? 
+            embed.setDescription(`${client.lang('SLOWMODE_DESACTIVATE', mention)}`) 
+            : embed.setDescription(`${client.lang('SLOWMODE_ACTIVATE', duration)}\n**${client.lang('CHANNEL')}**: <#${mention}>`);
 
         if(duration == 'off') duration = '0';
 
         if(regex.test(duration) !== true || !duration) return message.error(client.lang('NO_VALID_DURATION'), { type: 'embed' } );
-        if(!channel || channel.type === 'GUILD_VOICE') return message.error(client.lang('INVALID_CHANNEL'), { type: 'embed' } );
+        if(mention) {
+            let channel = message.guild.channels.cache.get(mention);
+            if(!channel || channel.type === 'GUILD_VOICE') return message.error(client.lang('INVALID_CHANNEL'), { type: 'embed' } );
+        }
 
         let time = ms(duration) / 1000;
 
-        if(!mention) {
-
-            message.channel.setRateLimitPerUser(time);
-
-        } else {
-
-            channel.setRateLimitPerUser(time);
-
-        }
-
-        if(duration == 'off' || duration == '0s') {
-
-            embed.setDescription(`${client.lang('SLOWMODE_DESACTIVATE', mention)}`)
-
-        } else {
-
-            embed.setDescription(`${client.success} ${client.lang('SLOWMODE_ACTIVATE', duration)}\n**${client.lang('CHANNEL')}**: <#${mention}>`)
-
-        }
+        !mention ? message.channel.setRateLimitPerUser(time) : channel.setRateLimitPerUser(time);
 
         embed.setTitle('Slowmode')
         embed.setTimestamp()
